2b7efdd86a8e5507890c3e05ca1dfdc97a27e8dc
Mcas4150 <mikecassidy0000@gmail.com>
2019-07-28 14:17:11 -0400

    master

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -11,8 +11,8 @@ export default class Canvas extends Component {
   componentDidMount() {
     this.updateWindowDimensions();
     window.addEventListener("resize", this.updateWindowDimensions);
-    // let globalId = window.setTimeout(this.updateCanvas, 1500);
-    let globalId = window.requestAnimationFrame(this.updateCanvas);
+    let globalId = window.setTimeout(this.updateCanvas, 500);
+    // let globalId = window.requestAnimationFrame(this.updateCanvas);
     this.setState({
       globalId: globalId
     });
@@ -23,37 +23,27 @@ export default class Canvas extends Component {
     window.removeEventListener("resize", this.updateWindowDimensions);
   }
 
+
+
+
   updateCanvas() {
     let ctx = this.refs.canvas.getContext("2d");
     var time = new Date();
+    let angle = 0;
 
-    // const earth = {
-    //   x: 500,
-    //   y: 300,
-    //   radius: 25,
-    //   color: "blue",
-    //   draw: function() {
-    //     ctx.beginPath();
-    //     ctx.rotate(
-    //       ((2 * Math.PI) / 60) * time.getSeconds() +
-    //         ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    //     );
-    //     ctx.translate(105, 0);
-    //     ctx.fillRect(0, -12, 40, 24); // Shadow
 
-    //     ctx.translate(10, 10);
-    //     ctx.closePath();
-    //     // ctx.rotate(
-    //     //   ((2 * Math.PI) / 60) * time.getSeconds() +
-    //     //     ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    //     // );
-    //     ctx.fillStyle = this.color;
-    //     ctx.fill();
-    //   },
-    //   rotate: function() {}
-    // };
 
-    ctx.globalCompositeOperation = "multiple";
+    const orbit = {
+      draw: function() {
+        ctx.beginPath();
+        ctx.arc(550, 460, 135, 0, Math.PI * 2, false); // Earth orbit
+        ctx.fillStyle = "deepskyblue";
+        ctx.stroke();
+      },
+      rotate: function() {}
+    };
+
+    // ctx.globalCompositeOperation = "destination-over";
     ctx.clearRect(0, 0, 2000, 2000); // clear canvas
 
     ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
@@ -73,19 +63,20 @@ export default class Canvas extends Component {
     ctx.translate(50, 0);
     ctx.beginPath();
     ctx.arc(20, 20, 8, 0, Math.PI * 2, true);
-    ctx.fillStyle = "turquoise";
+    ctx.fillStyle = "deepskyblue";
     ctx.stroke();
     ctx.fill();
 
     ctx.translate(60, -60);
     ctx.beginPath();
-    ctx.arc(20, 20, 15, 0, Math.PI * 2, true);
+    ctx.arc(20, 20, 15 * Math.abs(Math.cos(angle)), 0, Math.PI * 2, true);
+    ctx.fillStyle = "deepskyblue";
     ctx.stroke();
 
     ctx.translate(30, -85);
     ctx.beginPath();
-    ctx.arc(20, 20, 15, 0, Math.PI * 2, true);
-    ctx.fillStyle = "turquoise";
+    ctx.arc(20, 20, 15 * Math.abs(Math.cos(angle)), 0, Math.PI * 2, true);
+    ctx.fillStyle = "deepskyblue";
     ctx.stroke();
     ctx.fill();
 
@@ -93,20 +84,25 @@ export default class Canvas extends Component {
 
     ctx.moveTo(800, 300);
     ctx.lineTo(200, 100);
-    ctx.fillStyle = "turquoise";
+    ctx.fillStyle = "deepskyblue";
     ctx.restore();
 
     ctx.restore();
-    // window.setTimeout(ball.draw(), 3000);
+    window.setTimeout(orbit.draw(), 1500);
+
+    // ctx.beginPath();
+    // ctx.arc(550, 460, 135, 0, Math.PI * 2, false); // Earth orbit
+    // ctx.stroke();
 
     ctx.beginPath();
-    ctx.arc(550, 460, 135, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(550, 460, 75, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
+    // let arcX = 90;
+    // let arcY = 0;
 
+    // ctx.arc(arcX, arcY, 75, 0, Math.PI * 2, false); // Earth orbit
+    // ctx.stroke();
+    // arcX += 1;
+    // arcY += 1;
     ctx.beginPath();
     ctx.arc(550, 460, 200, Math.PI * 2, false); // Earth orbit
     ctx.stroke();
@@ -118,17 +114,50 @@ export default class Canvas extends Component {
     ctx.restore();
     ctx.moveTo(550, 460);
     ctx.lineTo(250, 100);
-    ctx.strokeStyle = "turquoise";
+    ctx.strokeStyle = "deepskyblue";
     ctx.stroke();
 
-     ctx.restore();
+    ctx.restore();
     ctx.moveTo(550, 460);
     ctx.lineTo(850, 100);
-    ctx.strokeStyle = "turquoise";
+    ctx.strokeStyle = "deepskyblue";
     ctx.stroke();
 
-    const globalId = window.requestAnimationFrame(this.updateCanvas);
-    this.setState({ globalId: globalId });
+    ctx.restore();
+
+    ctx.moveTo(550, 460);
+    ctx.lineTo(130, 460);
+    ctx.strokeStyle = "deepskyblue";
+    ctx.stroke();
+
+    ctx.restore();
+
+    ctx.moveTo(550, 460);
+    ctx.lineTo(900, 460);
+    ctx.strokeStyle = "deepskyblue";
+    ctx.stroke();
+
+    ctx.restore();
+
+    ctx.moveTo(550, 460);
+    ctx.lineTo(840, 800);
+    ctx.strokeStyle = "deepskyblue";
+    ctx.stroke();
+
+    ctx.restore();
+
+    ctx.moveTo(550, 460);
+    ctx.lineTo(275, 750);
+    ctx.strokeStyle = "deepskyblue";
+    ctx.stroke();
+
+    ctx.restore();
+
+
+
+      const globalId = window.requestAnimationFrame(this.updateCanvas);
+      this.setState({ globalId: globalId });
+
   }
 
   updateWindowDimensions() {
